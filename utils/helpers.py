bae7e42a907c2ef4df1e217238b0bca6d3ebc39e
Tcooper4 <tacooper608@gmail.com>
2025-05-03 15:07:45 -0400

    Fix Adj Close error in yfinance

--- utils/helpers.py
+++ utils/helpers.py
@@ -6,7 +6,7 @@ import yfinance as yf
 
 def fetch_price_data(symbol, period="6mo", interval="1d"):
     try:
-        df = yf.download(symbol, period=period, interval=interval, progress=False)
+        df = yf.download(symbol, period=period, interval=interval, progress=False, auto_adjust=False)
         df = df[["Open", "High", "Low", "Close", "Adj Close", "Volume"]]
         df.dropna(inplace=True)
         return df
