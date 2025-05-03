f870b5f51a7a8baa8dd756867d20c0cc9cb09cef
Tcooper4 <tacooper608@gmail.com>
2025-05-03 15:18:37 -0400

    Fix Adj Close error in yfinance

--- utils/sp500_tickers.py
+++ utils/sp500_tickers.py
@@ -1,8 +1,16 @@
-import requests
 import pandas as pd
+import requests
+from io import StringIO
 
 def get_sp500_tickers():
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    html = requests.get(url).text
-    df = pd.read_html(html)[0]
-    return df["Symbol"].tolist()
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise ValueError("Failed to fetch S&P 500 tickers from Wikipedia")
+
+    # Use StringIO to suppress the FutureWarning
+    tables = pd.read_html(StringIO(response.text))
+    df = tables[0]
+    
+    # Return a clean list of ticker strings
+    return df["Symbol"].astype(str).str.replace(".", "-", regex=False).tolist()
